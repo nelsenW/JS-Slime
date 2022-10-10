56eb3cb934fbbb6514bd41af0c0291c444cf1972
nelsenw <wnelsen9@gmail.com>
2022-10-10 10:02:37 -0700

    css editing

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import GameView from "./scripts/game_view.js"
 
 
 document.addEventListener("DOMContentLoaded", () =>{
-    const startGame = document.getElementById("startgame")
-    const startMenu = document.querySelector(".start-menu")
+    const startGame = document.getElementById("startgame");
+    const startMenu = document.querySelector(".start-menu");
+    const menuNav = document.querySelector(".menu-navbar");
+    const closeButton = document.getElementById("close-button")
+    const about = document.getElementById('about')
+    const aboutMenu = document.querySelector('.about-menu');
+    const controls = document.querySelector('#controls')
+    const controlsMenu = document.querySelector('.controls-menu')
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext('2d');
     const slimeColors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet', 'pink']
@@ -20,15 +26,31 @@ document.addEventListener("DOMContentLoaded", () =>{
     let slimeHomeColors = setInterval(() => {
         newGameView.game.slime.color = slimeColors[i % 7]
         i++ 
-    }, 500)
+    }, 500);
     
 
     
     startGame.addEventListener("click", () => {
-        clearInterval(slimeHomeColors)
-        newGameView.game.slime.color = 'blue'
-        startMenu.style.display = "none"
+        clearInterval(slimeHomeColors);
+        newGameView.game.slime.color = 'blue';
+        startMenu.style.display = "none";
         newGameView.bindKeyHandlers();
         newGameView.game.slime.gravity = 0.98
+    });
+
+    about.addEventListener("click", () => {
+        startMenu.style.display = 'none';
+        aboutMenu.style.display = 'flex';
+    })
+
+    controls.addEventListener("click", () => {
+        startMenu.style.display = 'none';
+        controlsMenu.style.display = 'flex';
+    })
+
+    closeButton.addEventListener("click", () =>{
+        aboutMenu.style.display = 'none';
+        startMenu.style.display = 'flex';
+        controlsMenu.style.display = 'none'
     })
 })
