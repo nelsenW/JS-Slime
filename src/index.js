c4f699e662513b21dfa0ab9e9cf10a7e604ab8e6
nelsenw <wnelsen9@gmail.com>
2022-10-12 16:19:02 -0700

    radius bug2

--- src/index.js
+++ src/index.js
@@ -36,8 +36,8 @@ document.addEventListener("DOMContentLoaded", () =>{
         newGameView.game.deaths++
         newGameView.game.score -= 1000; 
         newGameView.game.slime.health = newGameView.game.slime.roomHealth;
-        newGameView.game.level = new Level(ctx, canvas, LEVELS[newGameView.game.currentLevel], newGameView.game.slime)
         newGameView.game.slime.radius = newGameView.game.slime.radiusCheck()
+        newGameView.game.level = new Level(ctx, canvas, LEVELS[newGameView.game.currentLevel], newGameView.game.slime)
         newGameView.start(); 
         startFunc();
     });
